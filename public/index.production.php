d12ee73de19b168b337de77225e125d3e0a24265
feryanuar24 <feryanuar24@gmail.com>
2026-05-08 10:04:11 +0700

    fix(deploy): remove comments and rename files chore(deps): upgrade laravel-vite-plugin to 3.1.0 fix(deps): update symfony/http-foundation to 7.4.8 fix(index): improve production index.php for maintenance mode

--- public/index.production.php
+++ public/index.production.php
@@ -5,8 +5,13 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
+
 require __DIR__ . '/../../laravel/vendor/autoload.php';
 
+/** @var Application $app */
 $app = require_once __DIR__ . '/../../laravel/bootstrap/app.php';
 
 $app->handleRequest(Request::capture());
